22c284381d7f6f8a8460f0f34473f0737eaa1453
Andrew M <manuele.andrew+gitHub@gmail.com>
2019-06-15 14:07:41 -0400

    adding merge_sort

---
+++ algos/merge_sort.py
@@ -0,0 +1,53 @@
+"""
+Merge Sort:
+    Description: Merge Sort uses divide and conquer methodology to break down an array of items into 1 or 2 sized
+    arrays and sorts those smaller arrays. Then Merge Sort will comvine (or merge) those smaller arrays back together
+    to create a sorted array. Said another way, MergeSort is a divide and conquer algorithm that divides a list 
+    into equal halves until it has two single elements and then merges the sub-lists until the entire list has 
+    been reassembled in order.
+    
+    Runtime: O(log(n))
+    Spoce Complexity: O(n)
+"""
+
+def mergesort(items):
+
+    if len(items) <= 1:
+        return items
+    
+    mid = len(items) // 2
+    left = items[:mid]
+    right = items[mid:]
+    
+    left = mergesort(left)
+    right = mergesort(right)
+    
+    return merge(left, right)
+
+    
+def merge(left, right):
+    
+    merged = []
+    left_index = 0
+    right_index = 0
+    
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] > right[right_index]:
+            merged.append(right[right_index])
+            right_index += 1
+        else:
+            merged.append(left[left_index])
+            left_index += 1
+
+    merged += left[left_index:]
+    merged += right[right_index:]
+        
+    return merged
+
+
+test_list_1 = [8, 3, 1, 7, 0, 10, 2]
+test_list_2 = [1, 0]
+test_list_3 = [97, 98, 99]
+print('{} to {}'.format(test_list_1, mergesort(test_list_1)))
+print('{} to {}'.format(test_list_2, mergesort(test_list_2)))
+print('{} to {}'.format(test_list_3, mergesort(test_list_3)))
